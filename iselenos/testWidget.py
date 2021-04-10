dbcd461597d139ec0dd896ee7fdfa9c5985c3b58
Tiago Asta <tiagogil.asta@gmail.com>
2021-04-10 15:53:53 +0100

    Button Instancing
    Instancing

--- iselenos/testWidget.py
+++ iselenos/testWidget.py
@@ -5,19 +5,8 @@ import ipywidgets as widgets
 class testWidget:
     def __init__(self,a):
         self.x = a
-        tempwidget = widgets.IntSlider(value=a,min=0,max=11,step=1,description='Test:',disabled=False,continuous_update=False,orientation='horizontal',readout=True,readout_format='d')
-        self.widget = interact(self.select, x=tempwidget)
-        self.newWida = ""
-        
-        
+        self.widget = widgets.IntSlider(value=a,min=0,max=11,step=1,description='Test:',disabled=False,continuous_update=False,orientation='horizontal',readout=True,readout_format='d')
+
+
     def getWid(self):
         return self.widget
-
-    def select(self, x):
-        if x != self.x:
-            print("num")
-            display(self.newWid())
-            
-
-    def newWid(self):
-        return widgets.IntSlider(value=5,min=0,max=11,step=1,description='Test:',disabled=False,continuous_update=False,orientation='horizontal',readout=True,readout_format='d')
