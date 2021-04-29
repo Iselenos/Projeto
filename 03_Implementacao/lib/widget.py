9e173e9449cb279f6b9865e8b06518c60b650d45
Tiago Asta <tiagogil.asta@gmail.com>
2021-04-29 12:49:26 +0100

    Test

--- 03_Implementacao/lib/widget.py
+++ 03_Implementacao/lib/widget.py
@@ -2,5 +2,17 @@ import ipywidgets
 
 class Widget:
 
-    def test(self):
+    def widgetUpdate(self):
+        raise NotImplementedError
+
+    def getWidget(self):
+        raise NotImplementedError
+
+    def getAttributes(self):
+        raise NotImplementedError 
+
+    def getAttributesView(self):
+        raise NotImplementedError
+
+    def getReferenceButton(self):
         raise NotImplementedError
--- 03_Implementacao/lib/widgets/testWidget.py
+++ 03_Implementacao/lib/widgets/testWidget.py
@@ -1,10 +1,28 @@
-import ipywidgets
+import ipywidgets as widgets
 from ..widget import Widget
 
 class testWidget(Widget):
 
     def __init__(self):
         self.testValue = "test"
+        self.slider = widgets.IntSlider(
+                                        value=7,
+                                        min=0,
+                                        max=10,
+                                        step=1,
+                                        description='Test:',
+                                        disabled=False,
+                                        continuous_update=False,
+                                        orientation='horizontal',
+                                        readout=True,
+                                        readout_format='d'
+                                        )
 
-    def test(self):
-        print(self.testValue)
+    def widgetUpdate(self):
+        pass
+
+    def getWidget(self):
+        return self.slider
+
+    def getAttributes(self):
+        pass 
