e79049c7dbe0e6fcc750195ea9ffb6600544426f
Tiago Asta <tiagogil.asta@gmail.com>
2021-05-09 16:52:22 +0100

    09-05

--- 03_Implementacao/lib/widgets/testWidget.py
+++ 03_Implementacao/lib/widgets/testWidget.py
@@ -4,7 +4,7 @@ from ..widget import Widget
 class testWidget(Widget):
 
     def __init__(self):
-        self.testValue = "test"
+        #1st Initialize Widget itself
         self.slider = widgets.IntSlider(
                                         value=7,
                                         min=0,
@@ -17,12 +17,29 @@ class testWidget(Widget):
                                         readout=True,
                                         readout_format='d'
                                         )
+        #2nd Create Represent Button
+        self.represent = widgets.Button(
+            description= "Im a Int Slider",
+            disabled=False,
+            button_style='', 
+            tooltip='Click me',
+            icon='check'
+            )
+        #3rd Customize On Click Function
+        self.represent.on_click(on_button_clicked)
 
     def widgetUpdate(self):
         pass
+
+    def getReferenceButton(self):
+        return self.represent
+
 
     def getWidget(self):
         return self.slider
 
     def getAttributes(self):
         pass 
+
+def on_button_clicked(b):
+    b.description = "Test"
